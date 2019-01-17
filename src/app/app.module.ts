0d3423a7c1726f50efa62b68e5c068b506f8f6df
Hristijan112 <Hristijan112@DESKTOP-J67T9JJ>
2019-01-17 15:06:17 +0100

    design edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListsComponent } from './lists/lists.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
